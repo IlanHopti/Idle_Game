54aae5c2974e22e89d668f91f2372fefea6d7532
AyakorK <guillaume.moret@yahoo.com>
2023-10-03 23:02:40 +0200

    Refactor the navbar

--- Idle_Game/src/App.vue
+++ Idle_Game/src/App.vue
@@ -1,85 +1,30 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <header class="bg-blue-500 py-4">
+        <div class="container mx-auto">
+            <nav class="flex items-center justify-between">
+                <div class="text-white text-2xl font-bold">Mon Application</div>
+                <div class="space-x-4">
+                    <router-link to="/" class="text-white hover:text-blue-200">Home</router-link>
+                    <router-link to="/factories" class="text-white hover:text-blue-200">My Factories</router-link>
+                    <router-link to="/marketplace" class="text-white hover:text-blue-200">MarketPlace</router-link>
+                    <router-link to="/achievements" class="text-white hover:text-blue-200">Achievements</router-link>
+                    <router-link to="/register" class="text-white hover:text-blue-200">Register</router-link>
+                    <router-link to="/login" class="text-white hover:text-blue-200">Login</router-link>
+                </div>
+            </nav>
+        </div>
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <router-view />
 </template>
 
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
 }
 </style>

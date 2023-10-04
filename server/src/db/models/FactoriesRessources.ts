ee79f01be53e2c251b6ee0d336db09cc7cc9b984
Lucas Goncalves <lucasgoncalves@free.fr>
2023-10-04 23:26:25 +0200

    Create factoriesRessources

---
+++ server/src/db/models/FactoriesRessources.ts
@@ -0,0 +1,4 @@
+import { factoriesRessources } from "@/types/factoriesRessources.types";
+import { db } from "../mongo"
+
+export const FactoryRessources = db?.collection<factoriesRessources>('factoriesRessouce')
---
+++ server/src/types/factoriesRessources.types.ts
@@ -0,0 +1,15 @@
+export interface factoriesRessources {
+    ressources: RessourcesList[],
+    type: string
+}
+
+export interface RessourcesList{
+    wood: number,
+    money:number
+    coal:number
+    iron:number
+    stone:number
+    gold:number
+    diamond:number
+    [key:string]:number
+}

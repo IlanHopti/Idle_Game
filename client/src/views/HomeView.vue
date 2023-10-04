da356a95af1dcc136dceee2d673767871b9378e2
Ilan Petiot <91156471+IlanHopti@users.noreply.github.com>
2023-10-04 22:47:22 +0200

    Show dynamically factories

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,23 +1,48 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import FactoryItem from '../components/factoryItem.vue'
 import FactoryItemSkeleton from '../components/factoryItemSkeleton.vue'
+import { useFactoriesStore } from '@/stores/factories'
+import { useUserStore } from '@/stores/user'
+import WoodFactory from '@/assets/factories/wood_factory.png'
+import StoneFactory from '@/assets/factories/stone_factory.png'
+
+const userConnected = useUserStore()
+const factories = useFactoriesStore()
+
+if (userConnected.user.length == 0) {
+  console.log('fetching user')
+  userConnected.fetchUser().then(() => {
+    factories.fetchFactory(userConnected.user._id).then(() => {
+      console.log(factories.factories)
+    })
+  })
+}
 </script>
 
 <template>
   <main>
     <div class="flex flex-row justify-evenly">
       <div class="flex flex-col">
-        <FactoryItem
-          modal-id="woodFactory"
-          :factory-name="`Wood Factory`"
-          :factory-level="1"
-          borderColor="border-wood"
-          factory-img="https://cdn.discordapp.com/attachments/1158396116388302910/1159054969782145125/woodFactory-removebg-preview.png?ex=651e7d8e&is=651d2c0e&hm=a3ebf200d8e79c9c0d9021198985d43935d9296a873e9502b84469854dab9a58&"
-        />
-        <FactoryItemSkeleton :factory-name="`Wood Factory`" />
-        <FactoryItemSkeleton :factory-name="`Wood Factory`" />
-        <FactoryItemSkeleton :factory-name="`Wood Factory`" />
+        <div v-if="factories.factories.length == 0 || false" class="flex flex-col">
+          <FactoryItemSkeleton :factory-name="`Wood Factory`" />
+          <FactoryItemSkeleton :factory-name="`Wood Factory`" />
+          <FactoryItemSkeleton :factory-name="`Wood Factory`" />
+          <FactoryItemSkeleton :factory-name="`Wood Factory`" />
+        </div>
+        <div v-else class="flex flex-col">
+          <div v-for="factory in factories.factories" key="factory._id">
+            <FactoryItem
+              modal-id="woodFactory"
+              :factory-name="`Wood Factory`"
+              :factory-level="factory.level"
+              borderColor="border-wood"
+              :factory-img="WoodFactory"
+            />
+          </div>
+          <div v-for="n in 4 - factories.factories.length">
+            <FactoryItemSkeleton :factory-name="`Wood Factory`" />
+          </div>
+        </div>
       </div>
       <div class="flex flex-col">
         <FactoryItem
@@ -25,7 +50,7 @@ import FactoryItemSkeleton from '../components/factoryItemSkeleton.vue'
           :factory-name="`Stone Factory`"
           :factory-level="1"
           borderColor="border-stone"
-          factory-img="https://cdn.discordapp.com/attachments/1158396116388302910/1159054818787209258/48d073e4-982c-48c2-80b4-d28b2557abbe.jpeg?ex=651e7d6a&is=651d2bea&hm=5a0d995976351f7dacbb94e9b05ee12ac85d5a50caff787068a66dca525c88b8&"
+          :factory-img="StoneFactory"
         />
         <FactoryItemSkeleton :factory-name="`Stone Factory`" />
         <FactoryItemSkeleton :factory-name="`Stone Factory`" />

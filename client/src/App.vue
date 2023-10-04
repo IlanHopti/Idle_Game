5e4d120a00201c9ec18a7c70423a8a94270780b3
Ilan Petiot <91156471+IlanHopti@users.noreply.github.com>
2023-10-05 01:16:38 +0200

    format code

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,33 @@
 <template>
-    <header class="bg-black py-4">
-        <div class="container mx-auto">
-            <nav class="flex items-center justify-between">
-                <div class="text-white text-2xl font-bold">Mon Application</div>
-                <div class="space-x-4">
-                    <router-link to="/" class="text-white hover:text-blue-200">Home</router-link>
-                    <router-link to="/factories" class="text-white hover:text-blue-200">My Factories</router-link>
-                    <router-link to="/marketplace" class="text-white hover:text-blue-200">MarketPlace</router-link>
-                    <router-link to="/achievements" class="text-white hover:text-blue-200">Achievements</router-link>
-                    <template v-if="!isLogged">
-                        <router-link to="/register" class="text-white hover:text-blue-200">Register</router-link>
-                        <router-link to="/login" class="text-white hover:text-blue-200">Login</router-link>
-                    </template>
-                    <template v-else>
-                        <button @click="logout" class="text-white hover:text-blue-200">Logout</button>
-                    </template>
-                </div>
-            </nav>
+  <header class="bg-black py-4">
+    <div class="container mx-auto">
+      <nav class="flex items-center justify-between">
+        <div class="text-white text-2xl font-bold">Mon Application</div>
+        <div class="space-x-4">
+          <router-link to="/" class="text-white hover:text-blue-200">Home</router-link>
+          <router-link to="/factories" class="text-white hover:text-blue-200"
+            >My Factories</router-link
+          >
+          <router-link to="/marketplace" class="text-white hover:text-blue-200"
+            >MarketPlace</router-link
+          >
+          <router-link to="/achievements" class="text-white hover:text-blue-200"
+            >Achievements</router-link
+          >
+          <template v-if="!isLogged">
+            <router-link to="/register" class="text-white hover:text-blue-200"
+              >Register</router-link
+            >
+            <router-link to="/login" class="text-white hover:text-blue-200">Login</router-link>
+          </template>
+          <template v-else>
+            <button @click="logout" class="text-white hover:text-blue-200">Logout</button>
+          </template>
         </div>
-    </header>
-    <router-view />
+      </nav>
+    </div>
+  </header>
+  <router-view />
 </template>
 
 <script setup lang="ts">
@@ -31,27 +39,30 @@ const isLogged = ref(userConnected.isLogged)
 
 // Watch for changes in userConnected.isLogged and update isLogged.value
 onMounted(() => {
-  const unwatchIsLogged = watch(() => userConnected.isLogged, (newValue) => {
-    isLogged.value = newValue;
-  });
+  const unwatchIsLogged = watch(
+    () => userConnected.isLogged,
+    (newValue) => {
+      isLogged.value = newValue
+    }
+  )
 
   // Clean up the watcher when the component is unmounted
   onErrorCaptured(() => {
-    unwatchIsLogged();
-  });
-});
+    unwatchIsLogged()
+  })
+})
 
 // Fetch user data when the component is mounted
 onMounted(async () => {
   try {
-    await userConnected.fetchUser();
+    await userConnected.fetchUser()
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-});
+})
 
 const logout = () => {
-  userConnected.logout();
+  userConnected.logout()
 }
 </script>
 
--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -1,56 +1,56 @@
 <template>
-    <div class="bg-gray-100 min-h-screen flex items-center justify-center">
-        <div class="bg-white p-8 rounded-lg shadow-md max-w-md w-1/4">
-            <h2 class="text-2xl font-semibold mb-6">Inscription</h2>
-            <p class="text-xl font-semibold mb-6 text-red-500 animate-bounce">{{ errorMessage }}</p>
-            <form @submit.prevent="registerUser">
-                <div class="mb-4">
-                    <label for="username" class="block font-medium text-gray-700">Username</label>
-                    <input
-                            type="text"
-                            id="username"
-                            v-model="username"
-                            class="mt-1 p-2 w-full border rounded"
-                    />
-                </div>
-                <div class="mb-4">
-                    <label for="email" class="block font-medium text-gray-700">Email</label>
-                    <input type="email" id="email" v-model="email" class="mt-1 p-2 w-full border rounded" />
-                </div>
-                <div class="mb-6">
-                    <label for="password" class="block font-medium text-gray-700">Password</label>
-                    <input
-                            type="password"
-                            id="password"
-                            v-model="password"
-                            class="mt-1 p-2 w-full border rounded"
-                    />
-                </div>
-                <div class="mb-6">
-                    <label for="password" class="block font-medium text-gray-700">Confirm Password</label>
-                    <input
-                            type="password"
-                            id="confirm_password"
-                            v-model="confirm_password"
-                            class="mt-1 p-2 w-full border rounded"
-                    />
-                </div>
-                <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
-                    Register
-                </button>
-            </form>
-            <p class="mt-4">
-                Already member ? <router-link to="/login" class="text-blue-500">Log In</router-link>
-            </p>
+  <div class="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div class="bg-white p-8 rounded-lg shadow-md max-w-md w-1/4">
+      <h2 class="text-2xl font-semibold mb-6">Inscription</h2>
+      <p class="text-xl font-semibold mb-6 text-red-500 animate-bounce">{{ errorMessage }}</p>
+      <form @submit.prevent="registerUser">
+        <div class="mb-4">
+          <label for="username" class="block font-medium text-gray-700">Username</label>
+          <input
+            type="text"
+            id="username"
+            v-model="username"
+            class="mt-1 p-2 w-full border rounded"
+          />
         </div>
+        <div class="mb-4">
+          <label for="email" class="block font-medium text-gray-700">Email</label>
+          <input type="email" id="email" v-model="email" class="mt-1 p-2 w-full border rounded" />
+        </div>
+        <div class="mb-6">
+          <label for="password" class="block font-medium text-gray-700">Password</label>
+          <input
+            type="password"
+            id="password"
+            v-model="password"
+            class="mt-1 p-2 w-full border rounded"
+          />
+        </div>
+        <div class="mb-6">
+          <label for="password" class="block font-medium text-gray-700">Confirm Password</label>
+          <input
+            type="password"
+            id="confirm_password"
+            v-model="confirm_password"
+            class="mt-1 p-2 w-full border rounded"
+          />
+        </div>
+        <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+          Register
+        </button>
+      </form>
+      <p class="mt-4">
+        Already member ? <router-link to="/login" class="text-blue-500">Log In</router-link>
+      </p>
     </div>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import axios from 'axios'
-import router from "@/router";
-import {useUserStore} from "@/stores/user";
+import router from '@/router'
+import { useUserStore } from '@/stores/user'
 
 const username = ref('')
 const email = ref('')
@@ -59,24 +59,32 @@ const confirm_password = ref('')
 const errorMessage = ref('')
 
 const registeredUser = useUserStore()
-registeredUser.fetchUser().then(() => {
+registeredUser
+  .fetchUser()
+  .then(() => {
     if (registeredUser.user) {
-        router.push('/');
+      router.push('/')
     }
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err)
-})
+  })
 const registerUser = async () => {
-    try {
-        // Assuming connectUser.login returns a promise
-        const result = await registeredUser.register(email.value, username.value, password.value, confirm_password.value);
-        if (result.success) {
-            await router.push('/');
-        } else {
-            errorMessage.value = result.message;
-        }
-    } catch (error) {
-        errorMessage.value = 'An unexpected error occurred.';
+  try {
+    // Assuming connectUser.login returns a promise
+    const result = await registeredUser.register(
+      email.value,
+      username.value,
+      password.value,
+      confirm_password.value
+    )
+    if (result.success) {
+      await router.push('/')
+    } else {
+      errorMessage.value = result.message
     }
+  } catch (error) {
+    errorMessage.value = 'An unexpected error occurred.'
+  }
 }
 </script>

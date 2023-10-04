223c9349241bf1749aa7cd948811666bac7e3f68
Ilan Petiot <91156471+IlanHopti@users.noreply.github.com>
2023-10-04 17:47:20 +0200

    Remove localStorage and fix errors

--- client/src/stores/user.ts
+++ client/src/stores/user.ts
@@ -5,24 +5,23 @@ interface UserState {
 }
 
 export const useUserStore = defineStore('user', {
-  state: (): UserState =>
-    ({
-      user: []
-    }) as UserState,
+  state: (): UserState => ({
+    user: []
+  }),
   actions: {
     async fetchUser() {
-      if (localStorage.getItem('user')) {
-        const data = localStorage.getItem('user')
-        if (data) {
-          this.user = JSON.parse(data)
-        }
-      } else {
-        const data = await fetch('http://localhost:3001/user/651d624bc258b97e216800c7').then(
-          (res) => res.json()
-        )
-        console.log(data)
-        this.user = data.user
-      }
+      // if (localStorage.getItem('user')) {
+      //   const data = localStorage.getItem('user')
+      //   if (data) {
+      //     this.user = JSON.parse(data)
+      //   }
+      // } else {
+      const data = await fetch('http://localhost:3001/user/651d624bc258b97e216800c7').then((res) =>
+        res.json()
+      )
+      console.log(data)
+      this.user = data
+      // }
     }
   }
-} as const)
+})

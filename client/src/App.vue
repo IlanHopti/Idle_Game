0ef0670e69e40693f1bf2341130521290827c727
Théo Landemaine <91159174+TheoLandemaine@users.noreply.github.com>
2023-10-04 16:37:05 +0200

    Update navbar with logo & add inventory

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <header class="bg-blue-500 py-4">
+  <header class="bg-[#43c8b7] py-4">
     <div class="container mx-auto">
       <nav class="flex items-center justify-between">
-        <div class="text-white text-2xl font-bold">Mon Application</div>
+        <div class="flex flex-row items-center">
+          <img class="w-12 mr-4" src="../public/idle_logo.png" alt="idle_logo" />
+          <div class="text-white text-2xl font-bold">Idle Factory</div>
+        </div>
         <div class="space-x-4">
           <router-link to="/" class="text-white hover:text-blue-200">Home</router-link>
-          <router-link to="/factories" class="text-white hover:text-blue-200"
-            >My Factories</router-link
+          <router-link to="/inventory" class="text-white hover:text-blue-200"
+            >Inventory</router-link
           >
           <router-link to="/marketplace" class="text-white hover:text-blue-200"
             >MarketPlace</router-link

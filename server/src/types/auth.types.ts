2277c2c4d561ddef42a92eb9c82ee2971644253f
Lucas Goncalves <lucasgoncalves@free.fr>
2023-10-04 23:26:45 +0200

    Change types of users and factories

--- server/src/types/auth.types.ts
+++ server/src/types/auth.types.ts
@@ -1,16 +1,31 @@
+import { ObjectId } from "mongodb"
+
 export interface AuthRegisterBody {
     username: string
     password: string
 }
 
 export interface User {
+    _id?: ObjectId
     username: string
     password: string
     token: string
     createdAt: Date
+    money?: number
+    ressources?: UserRessources
 }
 
 export interface SimpleUser {
     username: string
     createdAt: Date
+}
+
+export interface UserRessources {
+    wood: number;
+    coal: number;
+    stone: number;
+    iron: number;
+    gold: number;
+    diamond: number;
+    [key: string]: number; // Ajouter cette signature d'index
 }

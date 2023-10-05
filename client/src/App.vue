2dcea29a08ca96ce0c63f200c6020470ddec950b
Théo Landemaine <91159174+TheoLandemaine@users.noreply.github.com>
2023-10-06 00:31:53 +0200

    fix resources

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,11 +9,27 @@
 <script setup lang="ts">
 import Navbar from './components/navbar.vue'
 import { useUserStore } from '@/stores/user'
-import { ref } from 'vue'
+import { ref, onMounted, onErrorCaptured, watch } from 'vue'
 import ResourcesComponent from '@/components/ResourcesComponent.vue'
 
 const userConnected = useUserStore()
 const isLogged = ref(userConnected.isLogged)
+
+// Watch for changes in userConnected.isLogged and update isLogged.value
+onMounted(() => {
+  const unwatchIsLogged = watch(
+    () => userConnected.isLogged,
+    (newValue) => {
+      console.log(newValue)
+      isLogged.value = newValue
+    }
+  )
+
+  // Clean up the watcher when the component is unmounted
+  onErrorCaptured(() => {
+    unwatchIsLogged()
+  })
+})
 </script>
 
 <style scoped>

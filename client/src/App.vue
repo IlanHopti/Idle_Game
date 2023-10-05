919b391070b5de4426d1108aef88052e2f62acb2
Théo Landemaine <91159174+TheoLandemaine@users.noreply.github.com>
2023-10-06 00:34:21 +0200

    fix navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,35 @@
 <template>
-  <Navbar />
+  <header class="bg-[#43c8b7] py-4">
+    <div class="container mx-auto">
+      <nav class="flex items-center justify-between">
+        <div class="flex flex-row items-center">
+          <img class="w-12 mr-4" src="../../public/idle_logo.png" alt="idle_logo" />
+          <div class="text-white text-2xl font-bold">Idle Factory</div>
+        </div>
+        <div class="space-x-4">
+          <router-link to="/" class="text-white hover:text-blue-200">Home</router-link>
+          <router-link to="/factories" class="text-white hover:text-blue-200"
+            >My Factories</router-link
+          >
+          <router-link to="/marketplace" class="text-white hover:text-blue-200"
+            >MarketPlace</router-link
+          >
+          <router-link to="/achievements" class="text-white hover:text-blue-200"
+            >Achievements</router-link
+          >
+          <template v-if="!isLogged">
+            <router-link to="/register" class="text-white hover:text-blue-200"
+              >Register</router-link
+            >
+            <router-link to="/login" class="text-white hover:text-blue-200">Login</router-link>
+          </template>
+          <template v-else>
+            <button @click="logout" class="text-white hover:text-blue-200">Logout</button>
+          </template>
+        </div>
+      </nav>
+    </div>
+  </header>
   <div v-if="isLogged">
     <ResourcesComponent></ResourcesComponent>
   </div>
@@ -39,6 +69,10 @@ onMounted(async () => {
     console.error(err)
   }
 })
+
+const logout = () => {
+  userConnected.logout()
+}
 </script>
 
 <style scoped>
